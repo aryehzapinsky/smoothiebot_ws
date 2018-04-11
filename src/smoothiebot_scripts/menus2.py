b0dec2360b178513d4f33f36012f65df963846a7
gh2434 <gh2434@columbia.edu>
2018-04-11 14:37:08 -0400

    added GraspManger to menus2 and its copy

--- src/smoothiebot_scripts/menus2.py
+++ src/smoothiebot_scripts/menus2.py
@@ -4,6 +4,7 @@ import abc
 import pyquaternion
 import numpy as np
 import graspit_interface.msg
+import curpp
 #import state
 #from skills import SkillManager
 #TODO STEP BY STEP:
@@ -40,6 +41,7 @@ def plan_grasps(x=0, y=0, z=0.080):
     grasps.append(construct_graspit_grasp(position, geom_orient_rot))
 
     return grasps
+
 def select_block():
     # type: (state.CRUIState, curpp.skills.SkillManager) -> ()
     rospy.loginfo("Entering select_block")
@@ -88,14 +90,183 @@ def run_recognition():
 
         # Return all detected blocks
         return detected_blocks
+class GraspManager:
+
+    def __init__(self):
+
+        # Initialize publishers
+
+        # Pull all params off param server
+        self.grasp_approach_tran_frame = rospy.get_param("grasp_approach_tran_frame")
+        self.world_frame = rospy.get_param("world_frame")
+        self.arm_move_group_name = rospy.get_param("arm_move_group_name")
+        self.gripper_move_group_name = rospy.get_param("gripper_move_group_name")
+
+        self.analyzer_planner_id = rospy.get_param("analyzer_planner_id")
+        self.executor_planner_id = rospy.get_param("executor_planner_id")
+        self.allowed_analyzing_time = rospy.get_param("allowed_analyzing_time")
+        self.allowed_execution_time = rospy.get_param("allowed_execution_time")
+
+        # Initialize ros service interfaces
+        self.grasping_controller = curpp.MoveitPickPlaceInterface(
+            arm_name=self.arm_move_group_name,
+            gripper_name=self.gripper_move_group_name,
+            grasp_approach_tran_frame=self.grasp_approach_tran_frame,
+            analyzer_planner_id=self.analyzer_planner_id,
+            execution_planner_id=self.executor_planner_id,
+            allowed_analyzing_time=self.allowed_analyzing_time,
+            allowed_execution_time=self.allowed_execution_time
+        )
+
+        self.scene = moveit_commander.PlanningSceneInterface()
+        self.world_manager_client = world_manager.world_manager_client.WorldManagerClient()
+        self.tf_listener = tf.TransformListener()
+
+    def graspit_grasp_to_moveit_grasp(self, object_name, graspit_grasp):
+        # type: (str, graspit_interface.msg.Grasp) -> moveit_msgs.msg.Grasp
+
+        pre_grasp_approach_direction = geometry_msgs.msg.Vector3Stamped()
+        pre_grasp_approach_direction.header.frame_id = rospy.get_param("pre_grasp_approach_direction_frame_id")
+        pre_grasp_approach_direction.vector.x = rospy.get_param("pre_grasp_approach_direction_x")
+        pre_grasp_approach_direction.vector.y = rospy.get_param("pre_grasp_approach_direction_y")
+        pre_grasp_approach_direction.vector.z = rospy.get_param("pre_grasp_approach_direction_z")
+
+        post_grasp_retreat_direction = geometry_msgs.msg.Vector3Stamped()
+        post_grasp_retreat_direction.header.frame_id = rospy.get_param("post_grasp_retreat_direction_frame_id")
+        post_grasp_retreat_direction.vector.x = rospy.get_param("post_grasp_retreat_direction_x")
+        post_grasp_retreat_direction.vector.y = rospy.get_param("post_grasp_retreat_direction_y")
+        post_grasp_retreat_direction.vector.z = rospy.get_param("post_grasp_retreat_direction_z")
+
+        grasp_approach_tran_frame = rospy.get_param("grasp_approach_tran_frame")
+
+        moveit_grasp_msg = curpp.graspit_grasp_to_moveit_grasp(
+            object_name=object_name,
+            graspit_grasp_msg=graspit_grasp,
+            listener=self.tf_listener,
+            grasp_tran_frame_name=grasp_approach_tran_frame,
+            end_effector_link=self.grasping_controller.get_end_effector_link(),
+
+            pre_grasp_goal_point_effort=rospy.get_param("pre_grasp_goal_point_effort"),
+            pre_grasp_goal_point_positions=rospy.get_param("pre_grasp_goal_point_positions"),
+            pre_grasp_goal_point_time_from_start_secs=rospy.get_param("pre_grasp_goal_point_time_from_start_secs"),
+            pre_grasp_joint_names=rospy.get_param("pre_grasp_joint_names"),
+
+            grasp_goal_point_effort=rospy.get_param("grasp_goal_point_effort"),
+            grasp_goal_point_positions=rospy.get_param("grasp_goal_point_positions"),
+            grasp_goal_point_time_from_start_secs=rospy.get_param("grasp_goal_point_time_from_start_secs"),
+
+            grasp_posture_joint_names=rospy.get_param("grasp_posture_joint_names"),
+
+            pre_grasp_approach_min_distance=rospy.get_param("pre_grasp_approach_min_distance"),
+            pre_grasp_approach_desired_distance=rospy.get_param("pre_grasp_approach_desired_distance"),
+            pre_grasp_approach_direction=pre_grasp_approach_direction,
+
+            post_grasp_retreat_min_distance=rospy.get_param("post_grasp_retreat_min_distance"),
+            post_grasp_retreat_desired_distance=rospy.get_param("post_grasp_retreat_desired_distance"),
+            post_grasp_retreat_direction=post_grasp_retreat_direction,
+
+            max_contact_force=rospy.get_param("max_contact_force")
+        )
+
+        return moveit_grasp_msg
+
+    def analyze_grasp_reachability(self, object_name, graspit_grasp):
+        # type: (str, graspit_interface.msg.Grasp) -> (moveit_msgs.msg.PickupResult, bool)
+        """
+        @return: Whether the grasp is expected to succeed
+        """
+        # Convert graspit grasp to moveit grasp
+        rospy.loginfo("Analyzing grasp for object: {}".format(object_name))
+
+        block_names = self.scene.get_attached_objects().keys()
+        self.grasping_controller.detach_all_blocks(block_names)
+
+        moveit_grasp_msg = self.graspit_grasp_to_moveit_grasp(object_name, graspit_grasp)
+        success, pick_result = self.grasping_controller.analyze_moveit_grasp(object_name, moveit_grasp_msg)
+
+        rospy.loginfo("Able to execute grasp with grasp id {} after analysis: {}".format(moveit_grasp_msg.id, success))
+
+        return pick_result, success
+
+    def execute_grasp(self, object_name, graspit_grasp, place_position=None):
+        # type: (str, graspit_interface.msg.Grasp) -> bool
+        rospy.loginfo("Executing grasp goal")
+
+        block_names = self.scene.get_attached_objects().keys()
+        self.grasping_controller.detach_all_blocks(block_names)
+
+        # Acquire block position for place
+        objects = self.scene.get_object_poses([object_name])
+        if object_name not in objects:
+            rospy.logerr("Object {} not in planning scene. Execute grasp failed".format(object_name))
+            return False
+
+        block_pose_stamped = geometry_msgs.msg.PoseStamped()
+        block_pose_stamped.pose = objects[object_name]
+        block_pose_stamped.header.frame_id = self.grasping_controller.get_planning_frame()
+
+        rospy.loginfo("Object {} in planning scene. Pose: {}".format(object_name, block_pose_stamped.pose))
+
+        # Shift block pose to place location in param server
+        if place_position is not None:
+            block_pose_stamped.pose.position.x = rospy.get_param("final_block_position_x")
+            block_pose_stamped.pose.position.y = rospy.get_param("final_block_position_y")
+            block_pose_stamped.pose.position.z = rospy.get_param("final_block_position_z")
+        else:
+            block_pose_stamped.pose.position = place_position
+
+        # Convert graspit grasp to moveit grasp
+        moveit_grasp_msg = self.graspit_grasp_to_moveit_grasp(object_name, graspit_grasp)
+
+        # Execute pick on block
+        success, pick_result = self.grasping_controller.execute_moveit_grasp(object_name, moveit_grasp_msg)
+        # type: pick_result -> moveit_msgs.msg.PickupResult
+
+        if not success:
+            error_code = curpp.moveit_error_code_to_string(pick_result.error_code)
+            rospy.logerr("Failed to execute pick. Reason: {}".format(error_code))
+            return False
+        else:
+            rospy.loginfo("Successfully executed pick")
+
+        rospy.loginfo("Placing block as position ({}, {}, {})"
+                      .format(block_pose_stamped.pose.position.x,
+                              block_pose_stamped.pose.position.y,
+                              block_pose_stamped.pose.position.z))
+        # Execute place on block
+        success, place_result = self.grasping_controller.place(object_name, pick_result, block_pose_stamped)
+
+        if not success:
+            error_code = curpp.moveit_error_code_to_string(place_result.error_code)
+            rospy.logerr("Failed to execute place. Reason: {}".format(error_code))
+            return False
+        else:
+            rospy.loginfo("Successfully executed place")
+
+        # Home arm and open hand
+        success = self.grasping_controller.home_arm()
+        if not success:
+            rospy.logerr("Failed to home arm")
+            return False
+        else:
+            rospy.loginfo("Successfully homed arm")
+
+        success = self.grasping_controller.open_hand()
+        if not success:
+            rospy.logerr("Failed to open hand")
+            return False
+        else:
+            rospy.loginfo("Successfully opened hand")
+
+        return True
 
 if __name__ =="__main__":
 	detected_blocks = select_block()
-	
+	manager = GraspManager()
 	#plan grasp reachability
 	for block in detected_blocks:
-		plan_grasps(block.pose.position.x, block.pose.position.y, block.pose.position.z)
-
+		grasps = plan_grasps(block.pose.position.x, block.pose.position.y, block.pose.position.z)
+		manager.analyze_grasp_reachability(block, grasps)
 	
  
 	
