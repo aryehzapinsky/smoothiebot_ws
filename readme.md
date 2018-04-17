bf0ab8307ee32ba6d909acf8edaec7dbbc3ad105
Aryeh Zapinsky <aryeh.zapinsky@columbia.edu>
2018-04-17 19:58:09 -0400

    everything launches robot, need to delay the add to wm

--- readme.md
+++ readme.md
@@ -18,3 +18,11 @@ source devel/setup.zsh
 source devel/setup.bash
 
 roslaunch launcher everything.launch
+
+
+# You can run everything.launch
+
+rosrun smoothiebot add_random_fruits.py
+
+
+add_random_fruits have the poses that you need
--- src/smoothiebot/scripts/add_random_fruits.py
+++ src/smoothiebot/scripts/add_random_fruits.py
@@ -1,17 +1,33 @@
+#!/usr/bin/env python
+
 import world_manager
 import geometry_msgs.msg
 
-wm = world_manager.world_manager_client.WorldManagerClient()
 
-pose_stamped = geometry_msgs.msg.PoseStamped()
-pose_stamped.pose = geometry_msgs.msg.Pose()
-pose_stamped.pose.position.x = 1.0
-pose_stamped.pose.position.y = 1.0
-pose_stamped.pose.position.z = 1.0
-pose_stamped.pose.orientation.x = 1.0
-pose_stamped.pose.orientation.y = 1.0
-pose_stamped.pose.orientation.z = 1.0
-pose_stamped.pose.orientation.w = 1.0
-pose_stamped.header.frame_id = "apple"
+def fill_pose_stamped(x, y, z, ox, oy, oz, ow):
+    pose_stamped = geometry_msgs.msg.PoseStamped()
+    pose_stamped.pose.position.x = x
+    pose_stamped.pose.position.y = y
+    pose_stamped.pose.position.z = z
+    pose_stamped.pose.orientation.x = ox
+    pose_stamped.pose.orientation.y = oy
+    pose_stamped.pose.orientation.z = oz
+    pose_stamped.pose.orientation.w = ow
+    pose_stamped.header.frame_id = "base_link"
 
-#wm.add_mesh("apple", mesh_filepath='/home/ozymandias/smoothiebot_ws/Fruit/apple.ply', pose_stamped=pose_stamped)
+    return pose_stamped
+
+if __name__ == "__main__":
+    wm = world_manager.world_manager_client
+    print("World manager client created")
+
+    apple_pose_stamped = fill_pose_stamped(.4, .2, .5, 0, 0, 0, 1.0)
+    wm.add_mesh("apple", mesh_filepath='/home/ozymandias/smoothiebot_ws/FruitPlys/apple.ply', pose_stamped=apple_pose_stamped)
+    print("add banana")
+
+    banana_pose_stamped = fill_pose_stamped(.4, -.2, .5, 1.0, 0, 0, 1.0)
+    wm.add_mesh("banana", mesh_filepath='/home/ozymandias/smoothiebot_ws/FruitPlys/banana.ply', pose_stamped=banana_pose_stamped)
+
+    # Need to scale orange file properly
+    #orange_pose_stamped = fill_pose_stamped(.5, 0, .5, 0, 0, 0, 1.0)
+    #wm.add_mesh("orange", mesh_filepath='/home/ozymandias/smoothiebot_ws/FruitPlys/orange.ply', pose_stamped=orange_pose_stamped)
