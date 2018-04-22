4e2b43f9b2ff8c4680f5612c56fa2dbeaf2f0412
Aryeh Zapinsky <aryeh.zapinsky@columbia.edu>
2018-04-22 19:18:13 -0400

    Fruit and transforms properly placed
    need to place table in future

--- src/smoothiebot/scripts/add_random_fruits.py
+++ src/smoothiebot/scripts/add_random_fruits.py
@@ -3,8 +3,27 @@
 import world_manager
 import geometry_msgs.msg
 import rospy
+import tf
 
-def fill_pose_stamped(x, y, z, ox, oy, oz, ow):
+import world_transform
+
+"""
+tf publisher
+publisher position x: 1, y: 1, z:1, orientartion
+publish tf from /world to /base_link
+publish transform /tf from /world -> /baselink
+rospy rate 50 hertz
+
+"""
+
+def copy_to_pose_stamped(pose, object_name):
+    pose_stamped = geometry_msgs.msg.PoseStamped()
+    pose_stamped.pose = pose
+    pose_stamped.header.frame_id = object_name
+    return pose_stamped
+
+def fill_pose_stamped(x, y, z, ox, oy
+                      , oz, ow):
     pose_stamped = geometry_msgs.msg.PoseStamped()
     pose_stamped.pose.position.x = x
     pose_stamped.pose.position.y = y
@@ -13,7 +32,7 @@ def fill_pose_stamped(x, y, z, ox, oy, oz, ow):
     pose_stamped.pose.orientation.y = oy
     pose_stamped.pose.orientation.z = oz
     pose_stamped.pose.orientation.w = ow
-    pose_stamped.header.frame_id = "base_link"
+    pose_stamped.header.frame_id = "world"
 
     return pose_stamped
 
@@ -24,11 +43,16 @@ if __name__ == "__main__":
     wm = world_manager.world_manager_client
     print("World manager client created")
 
-    apple_pose_stamped = fill_pose_stamped(0.696731, 0.014137, 0.77531, 1.58409, -0.039766, 0.000459, 1.0)
+    listener = tf.listener.TransformListener()
+    listener.waitForTransform("/base_link", "/world", rospy.Time(), rospy.Duration(4.0))
+    #apple_pose_stamped = fill_pose_stamped(0.696731, 0.014137, 0.77531, 0.71031, -0.01339, 0.013910, 0.70362)
+    apple_pose = world_transform.get_model_pose("apple")
+    apple_pose_stamped = copy_to_pose_stamped(apple_pose, "world")
     wm.add_mesh("apple", mesh_filepath='/home/ozymandias/smoothiebot_ws/FruitPlys/apple.ply', pose_stamped=apple_pose_stamped)
     print("apple has been added")
 
-    banana_pose_stamped = fill_pose_stamped(0.633429, 0.193047, 0.784009, 1.56044, -0.003598, 0.000204, 1.0)
+    #banana_pose_stamped = fill_pose_stamped(0.633429, 0.193047, 0.784009, 0.702476, -0.002126, 0.002249, 0.711700)
+    banana_pose_stamped = copy_to_pose_stamped(world_transform.get_model_pose("banana"), "world")
     wm.add_mesh("banana", mesh_filepath='/home/ozymandias/smoothiebot_ws/FruitDae/banana.dae', pose_stamped=banana_pose_stamped)
     print("banana has been added")
 
---
+++ src/smoothiebot/scripts/world_transform.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+
+import rospy
+import geometry_msgs.msg
+import gazebo_msgs.msg
+import gazebo_msgs.srv
+import world_manager
+import tf
+import numpy as np
+
+def get_world_properties():
+    service_proxy = rospy.ServiceProxy("/gazebo/get_world_properties", gazebo_msgs.srv.GetWorldProperties)
+    service_proxy.wait_for_service(timeout=5)
+    service_proxy()
+
+def get_model_pose(object_name, object_entity=None):
+    service_proxy = rospy.ServiceProxy("/gazebo/get_model_state", gazebo_msgs.srv.GetModelState)
+    service_proxy.wait_for_service(timeout=5)
+
+    model_state = gazebo_msgs.srv.GetModelStateRequest()
+    model_state.model_name = object_name
+    if object_entity is not None:
+        model_state.relative_entity_name = object_entity
+    return service_proxy(model_state).pose
+
+def copy_to_pose_stamped(pose, object_name):
+    pose_stamped = geometry_msgs.msg.PoseStamped()
+    pose_stamped.pose = pose
+    pose_stamped.header.frame_id = object_name
+    return pose_stamped
+
+def adjust_rotation(pose_stamped):
+    o = pose_stamped.pose.orientation
+    rotz = tf.transformations.quaternion_from_euler(0, 0, 1.5707) #90 deg
+    original = np.array([o.x, o.y, o.z, o.w])
+    rotated = tf.transformations.quaternion_multiply(rotz, original)
+    pose_stamped.pose.orientation.x = rotated[0]
+    pose_stamped.pose.orientation.y = rotated[1]
+    pose_stamped.pose.orientation.z = rotated[2]
+    pose_stamped.pose.orientation.w = rotated[3]
+    return pose_stamped
+
+if __name__ == "__main__":
+    rospy.init_node("world_transform_publisher")
+    rospy.wait_for_service("/world_manager/clear_objects")
+
+    wm = world_manager.world_manager_client
+    print("World manager client created")
+
+    r = rospy.Rate(10)
+    while not rospy.is_shutdown():
+        fetch_pose = get_model_pose("fetch", "base_link")
+        base_link_stamped = copy_to_pose_stamped(fetch_pose, "base_link")
+        base_link_stamped = adjust_rotation(base_link_stamped)
+        print("~~~~\n{}\n~~~~\n".format(fetch_pose))
+        wm.add_tf("world", base_link_stamped)
+        r.sleep()
