33e5fdcffa98f49c2f90da82a954126381620850
Aryeh Zapinsky <aryeh.zapinsky@columbia.edu>
2018-04-23 16:57:24 -0400

    trying to get robot rotated

---
+++ src/smoothiebot/scripts/rotate_robot.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import actionlib
+import rospy
+
+from math import sin, cos
+from moveit_python import (MoveGroupInterface,
+                           PlanningSceneInterface)
+from moveit_python.geometry import rotate_pose_msg_by_euler_angles
+
+#from control_msgs.msg import FollowJointTrajectoryAction, FollowJointTrajectoryGoal
+#from control_msgs.msg import PointHeadAction, PointHeadGoal
+#from grasping_msgs.msg import FindGraspableObjectsAction, FindGraspableObjectsGoal
+from geometry_msgs.msg import PoseStamped
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+from moveit_msgs.msg import PlaceLocation, MoveItErrorCodes
+#from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+
+# Move base using navigation stack
+class MoveBaseClient(object):
+
+    def __init__(self):
+        self.client = actionlib.SimpleActionClient("move_base", MoveBaseAction)
+        rospy.loginfo("Waiting for move_base...")
+        self.client.wait_for_server()
+
+    def goto(self, x, y, theta, frame="world"):
+        move_goal = MoveBaseGoal()
+        move_goal.target_pose.pose.position.x = x
+        move_goal.target_pose.pose.position.y = y
+        move_goal.target_pose.pose.orientation.z = sin(theta/2.0)
+        move_goal.target_pose.pose.orientation.w = cos(theta/2.0)
+        move_goal.target_pose.header.frame_id = frame
+        move_goal.target_pose.header.stamp = rospy.Time.now()
+
+        # TODO wait for things to work
+        self.client.send_goal(move_goal)
+        self.client.wait_for_result()
